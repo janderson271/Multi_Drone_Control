3b82856975a4d33a1e0b25876253ec48ed06b263
ragz <raghav_anand98@yahoo.com>
2018-12-01 17:56:03 -0800

    yay

--- src/drone_controller/src/MPCcontroller.py
+++ src/drone_controller/src/MPCcontroller.py
@@ -63,15 +63,15 @@ class MPCcontroller():
 		# Variable constraints
 		xl = [None]*nx
 		xu = [None]*nx
-		v = 0.15
-		for i in range(3,5):
-			xl[i] = -v
-			xu[i] =  v
+		#v = 0.15
+		#for i in range(3,5):
+		#	xl[i] = -v
+		#	xu[i] =  v
 		d = 15
 		for i in range(6,8):
 			xl[i] = -d*3.14/180
 			xu[i] =  d*3.14/180
-		dv = 20
+		dv = 10
 		for i in range(9,11):
 			xl[i] = -dv*3.14/180
 			xu[i] =  dv*3.14/180
@@ -95,7 +95,7 @@ class MPCcontroller():
 
 		# State Constraints
 		constraints += [X[:,0] == self.x0] # Initial Constraint
-		for k in range(1,n):
+		for k in range(4,n):
 			for i in range(nx):
 				if xl[i] is not None:
 					constraints += [xl[i] <= X[i,k]]
