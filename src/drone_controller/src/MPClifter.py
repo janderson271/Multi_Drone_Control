c6c8a9cb58806470af10ee4242583b779f9b3951
Jason <janderson271@berkeley.edu>
2018-12-13 23:00:57 -0800

    almost same actuations...some launch file params not ref, little better---this is the real one

--- src/drone_controller/src/MPClifter.py
+++ src/drone_controller/src/MPClifter.py
@@ -14,89 +14,88 @@ class MPCcontroller():
 					        ubar_v = np.zeros(( 4, 1)), \
 					          x0_v = np.zeros((12, 1))  ):
 
+		self.ts = ts
+
 		self.r = xbar_v[0:3].reshape((3,1))
 
-		P = np.diag(P)
-		Q = np.diag(Q)
-		R = np.diag(R)
+		self.P = np.eye(12)#np.diag(P)
+		self.Q = np.eye(12)#np.diag(Q)
+		self.R = np.eye(4)#np.diag(R)
 
 		l=0.033
+		self.l = l
 		m=0.032
-		inertia_xx = 16e-6
-		inertia_yy = inertia_xx
-		inertia_zz = 29e-6
-		k=0.01
+		self.m = m
+		self.inertia_xx = 16e-6
+		self.inertia_yy = self.inertia_xx
+		self.inertia_zz = 29e-6
+		k = 0.01
+		self.k=k
 
-		n = 10
-		self.n = n
-		g = 9.81
+		self.n = 10
+		self.g = 9.81
 
 		self.x0 = cvx.Parameter(12)
 		self.x0.value = x0_v.flatten()
 
-		nx = 12
-		nu = 4
+		self.nx = 12
+		self.nu = 4
 
 		# A discrete form 
-		A = np.zeros((12,12))
-		A[0:3,3:6] = np.eye(3)
-		A[3,7] = g
-		A[4,6] = -g
-		A[6:9,9:12] = np.eye(3)
-		A = np.eye(12) + ts*A
-		self.A = A
+		self.A = np.zeros((12,12))
+		self.A[0:3,3:6] = np.eye(3)
+		self.A[3,7] = self.g
+		self.A[4,6] = -self.g
+		self.A[6:9,9:12] = np.eye(3)
+		self.A = np.eye(12) + self.ts*self.A
 
 		# B discrete form
-		B = np.zeros((12,4))
-		B[5,:] = 1/m*np.array([1,1,1,1])
-		B[9:12,:] = np.array([[l,-l,-l,l],[-l,-l,l,l],[k,-k,k,-k]])
-		B[ 9,:] = B[ 9,:]/inertia_xx
-		B[10,:] = B[10,:]/inertia_yy
-		B[11,:] = B[11,:]/inertia_zz
-		B = ts*B
-		self.B = B
+		self.B = np.zeros((12,4))
+		self.B[5,:] = 1/m*np.array([1,1,1,1])
+		self.B[9:12,:] = np.array([[l,-l,-l,l],[-l,-l,l,l],[k,-k,k,-k]])
+		self.B[ 9,:] = self.B[ 9,:]/self.inertia_xx
+		self.B[10,:] = self.B[10,:]/self.inertia_yy
+		self.B[11,:] = self.B[11,:]/self.inertia_zz
+		self.B = self.ts*self.B
 
 		# Dynamics Affine Disturbance discrete form
-		Bg= np.zeros((12,1))
-		Bg[5] = -g
-		Bg= ts*Bg.flatten()
-		self.Bg= Bg
+		self.Bg= np.zeros((12,1))
+		self.Bg[5] = -self.g
+		self.Bg= self.ts*self.Bg.flatten()
 
 		# Force Disturbance Parameter
 		self.Fd = cvx.Parameter(3)
 		self.Fd.value = np.zeros((3,1)).flatten()
-		Bd = np.zeros((12,3))
-		Bd[3,0] = ts/m 
-		Bd[4,1] = ts/m
-		Bd[5,2] = ts/m
-		self.Bd = Bd
+		self.Bd = np.zeros((12,3))
+		self.Bd[3,0] = self.ts/self.m 
+		self.Bd[4,1] = self.ts/self.m
+		self.Bd[5,2] = self.ts/self.m
 
 		# Disturbance Observer 
 		self.x_obs = np.zeros((15,1))
-		C = np.eye(12)
-		Cd = np.zeros((12,3))
-		self.A_obs = np.block([[A,Bd],[np.zeros((3,12)),np.eye(3)]])
-		self.B_obs = np.block([[B],[np.zeros((3,4))]])
-		self.Bg_obs = np.block([[Bg.reshape((12,1))],[np.zeros((3,1))]])
-		self.C_obs = np.block([[C,Cd]])
+		self.C = np.eye(12)
+		self.Cd = np.zeros((12,3))
+		self.A_obs = np.block([[self.A,self.Bd],[np.zeros((3,12)),np.eye(3)]])
+		self.B_obs = np.block([[self.B],[np.zeros((3,4))]])
+		self.Bg_obs = np.block([[self.Bg.reshape((12,1))],[np.zeros((3,1))]])
+		self.C_obs = np.block([[self.C,self.Cd]])
 		poles = np.array([93.,94,95,96,97,98,99,100,101,102,103,104,105,106,107])/130
 		sys = scipy.signal.place_poles(self.A_obs.transpose(),self.C_obs.transpose(),poles)
 		self.L_obs = sys.gain_matrix.transpose()
-		self.R = R
 
 		# Disturbance Rejection
-		C = np.block([np.eye(3),np.zeros((3,9))])
-		self.A_track = np.block([[A-np.eye(12),B],[C,np.zeros((3,4))]])
+		self.C = np.block([np.eye(3),np.zeros((3,9))])
+		self.A_track = np.block([[self.A-np.eye(12),self.B],[self.C,np.zeros((3,4))]])
 
 		# MPC cost function
-		xbar = cvx.Parameter(12)
-		xbar.value = xbar_v.flatten()
-		ubar = cvx.Parameter(4)
-		ubar.value = ubar_v.flatten()
+		self.xbar = cvx.Parameter(12)
+		self.xbar.value = xbar_v.flatten()
+		self.ubar = cvx.Parameter(4)
+		self.ubar.value = ubar_v.flatten()
 
 		# Variable constraints
-		xl = [None]*nx
-		xu = [None]*nx
+		xl = [None]*self.nx
+		xu = [None]*self.nx
 		d = 15
 		for i in range(6,8):
 			xl[i] = -d*3.14/180
@@ -110,41 +109,52 @@ class MPCcontroller():
 		uu =  0.15*np.ones( 4).flatten()
 
 		# CVXPY Formulation
-		X = cvx.Variable((nx,n + 1))
-		U = cvx.Variable((nu,n))
+		self.X = cvx.Variable((self.nx,self.n + 1))
+		self.U = cvx.Variable((self.nu,self.n))
 
 		# Cost function
-		J = cvx.quad_form(X[:,n]-xbar,P)
-		for k in range(n):
-			J += cvx.quad_form(X[:,k]-xbar,Q) + cvx.quad_form(U[:,k]-ubar,R)
+		self.J = cvx.quad_form(self.X[:,self.n]-self.xbar,self.P)
+		#print(self.U[:,k].shape)
+		#print(self.ubar.shape)
+		#print(self.R.shape)
+		#print('X',self.X[:,k].shape)
+		#print('xbar',self.xbar)
+		#print('Q',self.Q)
+		#print(cvx.quad_form(self.X[:,k]-self.xbar,self.Q))
+		#print('U',self.U[:,k].shape)
+		#print('ubar',self.ubar)
+		#print('U',self.U)
+		#print(self.U[:,k].T*self.R*self.U[:,k])
+		#print(cvx.quad_form(self.U[:,k]-self.ubar,self.R))
+		#print('R',self.R)
+		#print('I',np.eye(4))
 
-		constraints = []
+		for k in range(self.n):
+			self.J += cvx.quad_form(self.X[:,k]-self.xbar,self.Q) + cvx.quad_form(self.U[:,k]-self.ubar,np.eye(4))
+
+		self.constraints = []
 		# Dynamic Constraints		
-		for k in range(0,n):
-			constraints += [X[:,k+1] == A*X[:,k] + B*U[:,k] + Bg+ Bd*self.Fd]	
+		for k in range(0,self.n):
+			self.constraints += [self.X[:,k+1] == self.A*self.X[:,k] + self.B*self.U[:,k] + self.Bg+ self.Bd*self.Fd]	
 
 		# State Constraints
-		constraints += [X[:,0] == self.x0] # Initial Constraint
-		for k in range(1,n):
-			for i in range(nx):
+		self.constraints += [self.X[:,0] == self.x0] # Initial Constraint
+		for k in range(1,self.n):
+			for i in range(self.nx):
 				if xl[i] is not None:
-					constraints += [xl[i] <= X[i,k]]
+					self.constraints += [xl[i] <= self.X[i,k]]
 				if xu[i] is not None:
-					constraints += [X[i,k] <= xu[i]]
+					self.constraints += [self.X[i,k] <= xu[i]]
 
 		# Input Constraints
-		for k in range(0,n):
-			for i in range(nu):
+		for k in range(0,self.n):
+			for i in range(self.nu):
 				if ul[i] is not None:
-					constraints += [ul[i] <= U[i,k]]
+					self.constraints += [ul[i] <=self. U[i,k]]
 				if uu[i] is not None:
-					constraints += [U[i,k] <= uu[i]]
+					self.constraints += [self.U[i,k] <= uu[i]]
 
-		self.X = X
-		self.U = U
-		self.xbar = xbar
-		self.ubar = ubar
-		self.problem = cvx.Problem(cvx.Minimize(J),constraints)
+		self.problem = cvx.Problem(cvx.Minimize(self.J),self.constraints)
 
 		self.counter = 1 # for when solver fails
 
@@ -155,6 +165,8 @@ class MPCcontroller():
 		self.x0.value = x0.flatten()
 
 		self.problem.solve()
+
+		return self.U[:,0].value
 
 		if self.problem.status == "optimal":
 			if self.counter != 1:
@@ -189,7 +201,7 @@ class MPCcontroller():
 
 		# solve for xtrack and utrack
 		xtut = cvx.Variable((16,1)) # xt stacked on ut
-		J = cvx.quad_form(xtut[12:16],self.R)
+		J = cvx.quad_form(xtut[12:16],np.eye(4))
 		constraints = [self.A_track*xtut + np.block([[self.Bg.reshape((12,1))],[np.zeros((3,1))]]) == np.block([[-self.Bd.dot(dhat)],[self.r]])]
 		problem = cvx.Problem(cvx.Minimize(J),constraints)
 		problem.solve()
